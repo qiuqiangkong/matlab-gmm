8ad25caff6aeb01e53e348d9fabd81d95af152c5
Qiuqiang Kong <Qiuqiang Kong>
2016-06-30 00:23:37 +0100

    use stat toolbox's kmeans instead of voicebox's kmeans

--- Gmm.m
+++ Gmm.m
@@ -13,6 +13,8 @@
 %           15-11-2015 Modify output format to mu: p*M, Sigma: p*p*M
 %           17-11-2015 Modify restart number to 1
 %           18-11-2015 add: if min(eig(Sigma)) < thresh, then + a*I
+%           28-06-2016 use stat toolbox's kmeans instead of voicebox's
+%           kmeans
 % -----------------------------------------------------------
 % Remarks: need voicebox (kmeans.m), error_ellipse.m
 % input:   
@@ -76,7 +78,8 @@ function [pi0, mu0, Sigma0] = Gmm_init_by_kmeans(X, mix_num, restart_num, cov_th
 [N,p] = size(X);
 err = inf;
 for i1 = 1:restart_num
-    [mu0_curr, err_curr, indi_curr] = kmeans(X, mix_num, 'f');
+    [indi_curr, mu0_curr, errs] = kmeans( X, mix_num );
+    err_curr = sum( errs );
     mu0_curr = mu0_curr';
     if err_curr < err
         err = err_curr;
--- README.txt
+++ README.txt
@@ -2,14 +2,11 @@ THIS IS AN OPEN SOURCE HMM TOOLBOX. IT IS FREE FOR INDIVIDUALS & RESEARCH.
 IF YOU ARE USING IT FOR COMMERCIAL USE, PLEASE CONTACT THE AUTHOR FIRST. 
 AUTHOR: QIUQIANG KONG
 CONTACT: qiuqiangkong@gmail.com
-CREATE: 25-11-2015
+CREATE: 25-11-2015 v0.1
+Modified: 28-06-2016 v0.2
 ==========================================
 
 Usage:
-1. Download voicebox toolbox
-2. run test_gmm.m
+1. run test_gmm.m
 
 ==========================================
-
-Q.A.
-1. if you met 'Error using fileparts'. Just run it another time, it will be fine. (Maybe a bug in voicebox)
